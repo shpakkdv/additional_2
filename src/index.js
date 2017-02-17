500e9092e630a993eefb8de92d3ddf73d14c678f
shpakkdv <shpak_kdv@mail.ru>
2017-02-17 18:26:59 +0300

    task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function flatten(array) {
-  // your solution
+	var newArray = [], temp;
+
+	if ( !(array instanceof Array) ) {
+		return [];
+	}
+
+
+	if (array.every( function(item) { return !(item instanceof Array) } )) {
+		return array;
+	}
+
+	for (let i = 0; i < array.length; i++) {
+		if (array[i] instanceof Array) {
+			temp = flatten(array[i]);
+			if (temp !== []) {
+				newArray = newArray.concat(temp);
+			}
+		} else {
+			newArray.push(array[i]);	
+		}
+	}
+	return newArray;
 }
